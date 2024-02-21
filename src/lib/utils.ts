c94c44eabe5cd00458cf0cc4ec25f89856e6a5f9
fabian-michael <fabian.michael9@icloud.com>
2024-02-21 22:22:10 +0100

    improve login page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,74 +4,69 @@ import qs from 'qs';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 export function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function trimEnd(str: string, char: string): string {
-    return str.replace(new RegExp(char + '+$'), '');
+	return str.replace(new RegExp(char + '+$'), '');
 }
 
 export function trimStart(str: string, char: string): string {
-    return str.replace(new RegExp('^' + char + '+'), '');
+	return str.replace(new RegExp('^' + char + '+'), '');
 }
 
 export function trim(str: string, char: string): string {
-    return trimStart(trimEnd(str, char), char);
+	return trimStart(trimEnd(str, char), char);
 }
 
 export const trimCurried = (char: string) => (str: string) => trim(str, char);
 
-export function buildUrl(parts: string[], trailingSlash?: boolean): string {
-    const url = parts
-        .filter(Boolean)
-        .map(trimCurried('/'))
-        .join('/');
+export function buildUrl(
+	parts: string[],
+	{ trailingSlash, leadingSlash }: { trailingSlash?: boolean; leadingSlash?: boolean } = {},
+): string {
+	const url = parts.filter(Boolean).map(trimCurried('/')).join('/');
 
-    return url + (trailingSlash ? '/' : '');
+	return (leadingSlash ? '/' : '') + url + (trailingSlash ? '/' : '');
 }
 
 export async function makePayloadRequest<T>({
-    slug,
-    fetch,
-    where,
-    init,
+	slug,
+	fetch,
+	where,
+	init,
 }: {
-    slug: string;
-    fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
-    init?: RequestInit;
-    where?: Record<string, any>;
+	slug: string;
+	fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
+	init?: RequestInit;
+	where?: Record<string, any>;
 }): Promise<T> {
-    const urlParts = [
-        PUBLIC_PAYLOAD_BASE,
-        slug,
-    ];
+	const urlParts = [PUBLIC_PAYLOAD_BASE, slug];
 
-    if (where) {
-        const queryString = qs.stringify(
-            {
-                where,
-            },
-            {
-                addQueryPrefix: true
-            },
-        );
+	if (where) {
+		const queryString = qs.stringify(
+			{
+				where,
+			},
+			{
+				addQueryPrefix: true,
+			},
+		);
 
-        urlParts.push(queryString);
-    }
+		urlParts.push(queryString);
+	}
 
-    const url = buildUrl(urlParts);
+	const url = buildUrl(urlParts);
 
+	const payloadResponse = await fetch(url, init);
 
-    const payloadResponse = await fetch(url, init);
+	if (!payloadResponse.ok) {
+		throw payloadResponse;
+	}
 
-    if (!payloadResponse.ok) {
-        throw payloadResponse;
-    }
-
-    return payloadResponse.json() as Promise<T>;
-
+	return payloadResponse.json() as Promise<T>;
 }
--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -5,26 +5,43 @@
 	import { zod } from 'sveltekit-superforms/adapters';
 	import { defaults } from 'sveltekit-superforms/client';
 	import { schema } from './schema.js';
+	import { createToaster, melt } from '@melt-ui/svelte';
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
 
 	export let data;
 
 	const form = defaults(zod(schema));
 	let status: 'initial' | 'idle' | 'loading' = 'initial';
-	let error = '';
+	// let error = '';
+
+	type ToastData = {
+		title: string;
+		description: string;
+		type: 'error';
+	};
+
+	const {
+		elements: { content, title, description, close },
+		helpers: { addToast },
+		states: { toasts },
+		actions: { portal },
+	} = createToaster<ToastData>();
 </script>
 
 <div class="fixed inset-0 grid items-end justify-center px-4 py-8 md:items-center">
-	<div class="max-w-sm card bg-neutral text-neutral-content">
+	<div class="max-w-sm w-screen card bg-neutral text-neutral-content">
 		<Form
 			data={form}
 			validationAdapter={zod(schema)}
 			action={async ({ form }) => {
+				// error = '';
+
 				if (!form.valid || status === 'loading') {
 					return;
 				}
 
 				try {
-					error = '';
 					status = 'loading';
 
 					await sleep(500);
@@ -46,9 +63,23 @@
 					console.error(err);
 
 					if (err instanceof Error) {
-						error = err.message;
+						// error = err.message;
+						addToast({
+							data: {
+								title: 'Login failed',
+								description: err.message,
+								type: 'error',
+							},
+						});
 					} else {
-						error = 'Something went wrong';
+						// error = 'Something went wrong';
+						addToast({
+							data: {
+								title: 'Login failed',
+								description: 'Something went wrong. Please try again later.',
+								type: 'error',
+							},
+						});
 					}
 				} finally {
 					status = 'idle';
@@ -57,8 +88,6 @@
 			class="card-body"
 			spa
 		>
-			<!-- <div class="justify-center card-title"><IconKey /> Login</div> -->
-
 			<Input
 				type="email"
 				id="login-email"
@@ -89,13 +118,53 @@
 						Login
 					{/if}
 				</button>
-
-				{#if error}
-					<p class="text-error">
-						{error}
-					</p>
-				{/if}
 			</div>
 		</Form>
 	</div>
+</div>
+
+<div
+	class="toast toast-end"
+	use:portal
+>
+	{#each $toasts as { id, data } (id)}
+		<div
+			use:melt={$content(id)}
+			animate:flip={{ duration: 500 }}
+			in:fly={{ duration: 150, x: '100%' }}
+			out:fly={{ duration: 150, x: '100%' }}
+			class="alert shadow-lg"
+			class:alert-error={data.type === 'error'}
+		>
+			<div>
+				<h3
+					use:melt={$title(id)}
+					class="font-semibold"
+				>
+					{data.title}
+				</h3>
+				<div use:melt={$description(id)}>
+					{data.description}
+				</div>
+			</div>
+			<button
+				use:melt={$close(id)}
+				class="btn btn-sm btn-error btn-circle"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-4 w-4"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/></svg
+				>
+			</button>
+		</div>
+	{/each}
 </div>
