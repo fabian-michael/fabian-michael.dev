c5b3766e67035b5b6ae9cec875f172b6112014e4
fabian-michael <fabian.michael9@icloud.com>
2024-03-06 20:13:38 +0100

    show progress on navigate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+	import { afterNavigate, onNavigate } from '$app/navigation';
 	import '../app.css';
 	import { Toaster } from '../lib/components/toaster';
 	import Navbar from './Navbar.svelte';
 
-	export let data;
+	let showProgress = $state(false);
+
+	onNavigate(() => {
+		showProgress = true;
+	});
+
+	afterNavigate(() => {
+		showProgress = false;
+	});
 </script>
+
+{#if showProgress}
+	<progress class="absolute inset-x-0 top-0 w-full h-0.5 progress progress-accent z-20"></progress>
+{/if}
 
 <Toaster>
 	<Navbar />
